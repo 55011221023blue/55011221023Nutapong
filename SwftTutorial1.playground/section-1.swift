434c8fe866f93954d29bd5c552e3e312e7007926
55011221023Nuttapong <student@tests-iMac-33.local>
2014-08-15 16:59:12 +0700

    bb
    bbb

--- SwftTutorial1.playground/section-1.swift
+++ SwftTutorial1.playground/section-1.swift
@@ -2,16 +2,16 @@ let tutorialTeam = 56
 let editorialTeam = 23
 let totalTeam = tutorialTeam + editorialTeam
 
-totalTeam += 1
+//totalTeam += 1
 
-let tutorialTeamInferred = 56
-let tutorialTeamExplicit: Int = 56
+let tutorialTeamInferred = -19
+let tutorialTeamExplicit: Int = -19
 
-let priceInferred = 19.99
-let priceExplicit:Double = 19.99
+let priceInferred = -19.99
+let priceExplicit:Double = -19.99
 
-let priceFloatInferred = 19.99
-let priceFloatExplicit:Float = 19.99
+let priceFloatInferred = -19.99
+let priceFloatExplicit:Float = -19.99
 
 let onSaleInferred = true
 let onSaleExplicit:Bool = false
