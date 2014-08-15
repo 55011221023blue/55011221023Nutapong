87e31e2a7bafd90fbcedd67702c15bf1c7a276ad
55011221023Nuttapong <student@tests-iMac-33.local>
2014-08-15 16:38:30 +0700

    edit
    edit

--- SwftTutorial1.playground/section-1.swift
+++ SwftTutorial1.playground/section-1.swift
@@ -1,6 +1,6 @@
 let tutorialTeam = 56
 let editorialTeam = 23
-var totalTeam = tutorialTeam + editorialTeam
+let totalTeam = tutorialTeam + editorialTeam
 
 totalTeam += 1
 
@@ -10,14 +10,12 @@ let tutorialTeamExplicit: Int = 56
 let priceInferred = 19.99
 let priceExplicit:Double = 19.99
 
+let priceFloatInferred = 19.99
+let priceFloatExplicit:Float = 19.99
+
 let onSaleInferred = true
 let onSaleExplicit:Bool = false
 
 let nameInferred = "Whoopie Cushion"
 let nameExplicit:String = "Whoopie Cushion"
 
-if (onSaleInferred) {
-    println("\(nameInferred) on sale for \(priceInferred)!")
-} else {
-    println("\(nameInferred) at regular price: \(priceInferred)!")
-}
