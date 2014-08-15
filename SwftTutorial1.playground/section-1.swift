cc0855dd6402382b0ee627122c66e563e31d9a0c
55011221023Nuttapong <student@tests-iMac-33.local>
2014-08-15 16:33:30 +0700

    edit
    edit

--- SwftTutorial1.playground/section-1.swift
+++ SwftTutorial1.playground/section-1.swift
@@ -1,3 +1,23 @@
-let tutorialTeas = 56
-let editorialtea = 23
-let xx = 0
+let tutorialTeam = 56
+let editorialTeam = 23
+var totalTeam = tutorialTeam + editorialTeam
+
+totalTeam += 1
+
+let tutorialTeamInferred = 56
+let tutorialTeamExplicit: Int = 56
+
+let priceInferred = 19.99
+let priceExplicit:Double = 19.99
+
+let onSaleInferred = true
+let onSaleExplicit:Bool = false
+
+let nameInferred = "Whoopie Cushion"
+let nameExplicit:String = "Whoopie Cushion"
+
+if (onSaleInferred) {
+    println("\(nameInferred) on sale for \(priceInferred)!")
+} else {
+    println("\(nameInferred) at regular price: \(priceInferred)!")
+}
