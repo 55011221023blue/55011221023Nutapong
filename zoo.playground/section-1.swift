4114c22929e25e2fe075030f18a8ed89d56a481a
55011212103Ice <student@tests-iMac-131.local>
2014-08-22 16:48:32 +0700

    www
    ww

--- zoo.playground/section-1.swift
+++ zoo.playground/section-1.swift
@@ -1,2 +1,2 @@
 var animall=["lion":"meat" , "Fish":"ซากูละ", "cow":"หญ้า", "บักเนม":"งัวทั้งตัว","hen":"ข้าวสาร",""]
-var user_data=["admin":"1234","the_name":"123456789"]
+var user_data = ["admin":"1234", "thename":"123456789"]
