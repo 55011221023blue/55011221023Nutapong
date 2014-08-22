d6ad77d0a0c1cf90c098e905ed02f2f631898707
55011212103Ice <student@tests-iMac-131.local>
2014-08-22 14:32:24 +0700

    dee
    dee

--- SwftTutorial1.playground/section-1.swift
+++ SwftTutorial1.playground/section-1.swift
@@ -20,3 +20,38 @@ let nameInferred = "Whoopie Cushion"
 let nameExplicit:String = "Whoopie"
 
 
+var shoppingListExplist = [String]()
+shoppingListExplist = ["Enggs","Milk"]
+//var shoppingListExplist: [String] = ["Eggs","Milk"]
+
+var shoppingList = ["Eggs","Milk"]
+println("the shopping list contains \(shoppingList.count) items")
+shoppingList.append("Flour")
+shoppingList += "Baking Power"
+shoppingList += ["Chocolat Spread" , "Cheese" , "Butter"]
+var firtItem = shoppingList[0]
+shoppingList[0...3] = ["Bananas" , "Apples"]
+shoppingList
+
+
+shoppingList += ["Baking Powder"]
+//shoppingList now contains 4 item
+
+shoppingList += ["chocolate spread" , "Cheese" , "Batter"]
+//shoppingList now contains 7 item
+
+shoppingList.insert("Maple Syrup" , atIndex:0)
+//shoppingList now contains 7 item
+//"Maple syrup" is now the first item in the List
+
+shoppingList.append("Flour")
+//shoppingList now contains 3 item , and someone is making panckes
+
+shoppingList += ["Baking Power"]
+
+var firstItem = shoppingList[0]
+//FirstItem is equal ti "eggs"
+
+shoppingList [4...6] = ["Bananas" , "Apples"]
+//shopinglist now contain 6 items
+
