0a99940266fffe3c3ba1d5a98d7b70461deb350e
losoteh@hotmail.com <student@tests-iMac-95.local>
2014-10-17 15:20:05 +0700

    updat
    updta

---
+++ segue/segue/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  segue
+//
+//  Created by Student on 10/17/14.
+//  Copyright (c) 2014 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var color_label: UILabel!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+   
+}
+
